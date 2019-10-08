964fd496918bccb5a96be11a6760809c52f5f364
Redclem <38690806+Redclem@users.noreply.github.com>
2019-10-08 15:52:13 +0200

    Add files via upload

---
+++ projet.py
@@ -0,0 +1,10 @@
+﻿import tkinter
+
+fenetre = tkinter.Tk()
+
+canv = tkinter.Canvas(fenetre)
+canv.grid(row = 0,column = 0)
+canv.grid_columnconfigure(0,weight=0)
+canv.grid_rowconfigure(0,weight=0)
+
+fenetre.mainloop()
