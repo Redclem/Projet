8bbfb7e8b98b872b67ad7a7bb5df821e739a6603
Redclem <38690806+Redclem@users.noreply.github.com>
2019-10-15 15:57:42 +0200

    Taille relative à l'affichage

--- projet.py
+++ projet.py
@@ -1,8 +1,9 @@
 ﻿from tkinter import *
-from enum import Enum
 
 CROIX = 1
 ROND = 2
+
+
 
 NOMS = ["", "Croix", "Rond"]
 
@@ -11,9 +12,11 @@ class Fenetre(Tk):
 
     def __init__(self):
         Tk.__init__(self)
+        self.LARGEUR_FENETRE = self.winfo_screenwidth() * 7 // 10
+        self.title("Morpion")
         self.resizable(False, False)
 
-        self.canv = Canvas(self, width=600, height=600, bg="gray")
+        self.canv = Canvas(self, width=self.LARGEUR_FENETRE * 3 // 4, height=self.LARGEUR_FENETRE * 3 // 4, bg="gray")
         self.canv.grid(row=0, column=0)
 
         self.frame_droite = Frame(self)
@@ -24,7 +27,7 @@ class Fenetre(Tk):
         self.bouton.grid(row=0, column=0, sticky=N + S + E + W)
         Grid.rowconfigure(self.frame_droite, 0, weight=1)
 
-        self.midframe = Frame(self.frame_droite, bg="black", width=200)
+        self.midframe = Frame(self.frame_droite, bg="black", width=self.LARGEUR_FENETRE // 4)
         self.midframe.grid(row=1, column=0, sticky=N + S + E + W)
         Grid.rowconfigure(self.frame_droite, 1, weight=1)
 
@@ -39,22 +42,22 @@ class Fenetre(Tk):
 
     def dessiner(self):
         self.canv.delete("all")
-        self.canv.create_line(200, 0, 200, 600, width=5)
-        self.canv.create_line(400, 0, 400, 600, width=5)
-        self.canv.create_line(0, 200, 600, 200, width=5)
-        self.canv.create_line(0, 400, 600, 400, width=5)
+        self.canv.create_line(self.LARGEUR_FENETRE // 4, 0, self.LARGEUR_FENETRE // 4, self.LARGEUR_FENETRE * 3 // 4, width=5)
+        self.canv.create_line(self.LARGEUR_FENETRE // 2, 0, self.LARGEUR_FENETRE // 2, self.LARGEUR_FENETRE * 3 // 4, width=5)
+        self.canv.create_line(0, self.LARGEUR_FENETRE // 4, self.LARGEUR_FENETRE * 3 // 4, self.LARGEUR_FENETRE // 4, width=5)
+        self.canv.create_line(0, self.LARGEUR_FENETRE // 2, self.LARGEUR_FENETRE * 3 // 4, self.LARGEUR_FENETRE // 2, width=5)
 
         for i in range(0, 9):
 
             if self.etatTableau[i // 3][i % 3] == CROIX:
-                self.canv.create_line((i // 3) * 200 + 10, (i % 3) * 200 + 10, (i // 3) * 200 + 190,
-                                      (i % 3) * 200 + 190, width=5)
-                self.canv.create_line((i // 3) * 200 + 10, (i % 3) * 200 + 190, (i // 3) * 200 + 190,
-                                      (i % 3) * 200 + 10, width=5)
+                self.canv.create_line((i // 3) * self.LARGEUR_FENETRE // 4 + 10, (i % 3) * self.LARGEUR_FENETRE // 4 + 10, (i // 3) * self.LARGEUR_FENETRE // 4 + (self.LARGEUR_FENETRE // 4 - 10),
+                                      (i % 3) * self.LARGEUR_FENETRE // 4 + (self.LARGEUR_FENETRE // 4 - 10), width=5)
+                self.canv.create_line((i // 3) * self.LARGEUR_FENETRE // 4 + 10, (i % 3) * self.LARGEUR_FENETRE // 4 + (self.LARGEUR_FENETRE // 4 - 10), (i // 3) * self.LARGEUR_FENETRE // 4 + (self.LARGEUR_FENETRE // 4 - 10),
+                                      (i % 3) * self.LARGEUR_FENETRE // 4 + 10, width=5)
 
             if self.etatTableau[i // 3][i % 3] == ROND:
-                self.canv.create_oval((i // 3) * 200 + 10, (i % 3) * 200 + 10, (i // 3) * 200 + 190,
-                                      (i % 3) * 200 + 190, width=5)
+                self.canv.create_oval((i // 3) * self.LARGEUR_FENETRE // 4 + 10, (i % 3) * self.LARGEUR_FENETRE // 4 + 10, (i // 3) * self.LARGEUR_FENETRE // 4 + (self.LARGEUR_FENETRE // 4 - 10),
+                                      (i % 3) * self.LARGEUR_FENETRE // 4 + (self.LARGEUR_FENETRE // 4 - 10), width=5)
 
     def reset(self, event=None):
         self.etatTableau = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
@@ -64,10 +67,10 @@ class Fenetre(Tk):
         self.victoire = False
 
         self.canv.delete("all")
-        self.canv.create_line(200, 0, 200, 600, width=5)
-        self.canv.create_line(400, 0, 400, 600, width=5)
-        self.canv.create_line(0, 200, 600, 200, width=5)
-        self.canv.create_line(0, 400, 600, 400, width=5)
+        self.canv.create_line(self.LARGEUR_FENETRE // 4, 0, self.LARGEUR_FENETRE // 4, self.LARGEUR_FENETRE * 3 // 4, width=5)
+        self.canv.create_line(self.LARGEUR_FENETRE // 2, 0, self.LARGEUR_FENETRE // 2, self.LARGEUR_FENETRE * 3 // 4, width=5)
+        self.canv.create_line(0, self.LARGEUR_FENETRE // 4, self.LARGEUR_FENETRE * 3 // 4, self.LARGEUR_FENETRE // 4, width=5)
+        self.canv.create_line(0, self.LARGEUR_FENETRE // 2, self.LARGEUR_FENETRE * 3 // 4, self.LARGEUR_FENETRE // 2, width=5)
 
         self.label.config(text=NOMS[self.joueur] + " commence")
 
@@ -75,8 +78,8 @@ class Fenetre(Tk):
         if (self.victoire):
             return
 
-        x = event.x // 200
-        y = event.y // 200
+        x = event.x // (self.LARGEUR_FENETRE // 4)
+        y = event.y // (self.LARGEUR_FENETRE // 4)
 
         if self.etatTableau[x][y] == 0:
             self.etatTableau[x][y] = self.joueur
@@ -84,7 +87,7 @@ class Fenetre(Tk):
             if self.test_victoire():
                 self.label.config(text=NOMS[self.joueur] + " a gagné!")
                 self.victoire = True
-            elif self.test_egalite:
+            elif self.test_egalite():
                 self.label.config(text="Égalité")
                 self.victoire = True
             else:
@@ -104,12 +107,10 @@ class Fenetre(Tk):
             if self.etatTableau[0][i] == self.etatTableau[1][i] and self.etatTableau[1][i] == self.etatTableau[2][i] and self.etatTableau[0][i] != 0:
                 return True
 
-        if (self.etatTableau[0][0] == self.etatTableau[1][1] and self.etatTableau[1][1] == self.etatTableau[2][2] and
-                self.etatTableau[0][0] != 0):
+        if (self.etatTableau[0][0] == self.etatTableau[1][1] and self.etatTableau[1][1] == self.etatTableau[2][2] and self.etatTableau[0][0] != 0):
             return True
 
-        if (self.etatTableau[2][0] == self.etatTableau[1][1] and self.etatTableau[1][1] == self.etatTableau[0][2] and
-                self.etatTableau[1][1] != 0):
+        if (self.etatTableau[2][0] == self.etatTableau[1][1] and self.etatTableau[1][1] == self.etatTableau[0][2] and self.etatTableau[1][1] != 0):
             return True
         return False
 
